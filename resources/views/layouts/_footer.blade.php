43e16e0ebe9aa29a8e02e8b7a632c9f455dccd55
zlsh09830123 <zlsh09830123@gmail.com>
2020-11-02 17:11:22 +0800

    Basic layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="#" target="_blank">Hans</a> 設計和開發 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">聯絡我們</a></p>
+  </div>
+</footer>
