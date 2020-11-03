7646ab09c1b70dbf7c4d8a717045b176e57383ee
zlsh09830123 <zlsh09830123@gmail.com>
2020-11-03 10:22:11 +0800

    Register & Login

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,23 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a class="nav-link" href="#">登入</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">註冊</a></li>
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+            <img src="https://img.icons8.com/fluent/30/000000/lemur.png" class="img-responsive">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="logout" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">登出</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="post" style="display: none;">
+              @csrf
+            </form>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
