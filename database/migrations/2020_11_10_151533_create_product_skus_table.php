ba2935ca69a04648802dcf9a50b4b232fb9ff85f
zlsh09830123 <zlsh09830123@gmail.com>
2020-11-10 16:10:42 +0800

    商品模型

---
+++ database/migrations/2020_11_10_151533_create_product_skus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSkusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_skus', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('stock');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_skus');
+    }
+}
