a0e7460bf72eb6dd9e32a5a90c4966dfafab79fd
zlsh09830123 <zlsh09830123@gmail.com>
2020-11-22 12:26:06 +0800

    導出管理後台資料

---
+++ database/seeders/AdminTablesSeeder.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace Database\Seeders;
+
+use Encore\Admin\Auth\Database\Menu;
+use Encore\Admin\Auth\Database\Permission;
+use Encore\Admin\Auth\Database\Role;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class AdminTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // base tables
+        Menu::truncate();
+        Menu::insert(
+            [
+                [
+                    "parent_id" => 0,
+                    "order" => 1,
+                    "title" => "首頁",
+                    "icon" => "fa-bar-chart",
+                    "uri" => "/",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 6,
+                    "title" => "系統管理",
+                    "icon" => "fa-tasks",
+                    "uri" => NULL,
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 7,
+                    "title" => "管理員",
+                    "icon" => "fa-users",
+                    "uri" => "auth/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 8,
+                    "title" => "角色",
+                    "icon" => "fa-user",
+                    "uri" => "auth/roles",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 9,
+                    "title" => "權限",
+                    "icon" => "fa-ban",
+                    "uri" => "auth/permissions",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 10,
+                    "title" => "菜單",
+                    "icon" => "fa-bars",
+                    "uri" => "auth/menu",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 11,
+                    "title" => "操作日誌",
+                    "icon" => "fa-history",
+                    "uri" => "auth/logs",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 2,
+                    "title" => "用戶管理",
+                    "icon" => "fa-users",
+                    "uri" => "/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 3,
+                    "title" => "商品管理",
+                    "icon" => "fa-cubes",
+                    "uri" => "/products",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 4,
+                    "title" => "訂單管理",
+                    "icon" => "fa-usd",
+                    "uri" => "/orders",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 5,
+                    "title" => "優惠券管理",
+                    "icon" => "fa-tags",
+                    "uri" => "/coupon_codes",
+                    "permission" => NULL
+                ]
+            ]
+        );
+
+        Permission::truncate();
+        Permission::insert(
+            [
+                [
+                    "name" => "All permission",
+                    "slug" => "*",
+                    "http_method" => "",
+                    "http_path" => "*"
+                ],
+                [
+                    "name" => "Dashboard",
+                    "slug" => "dashboard",
+                    "http_method" => "GET",
+                    "http_path" => "/"
+                ],
+                [
+                    "name" => "Login",
+                    "slug" => "auth.login",
+                    "http_method" => "",
+                    "http_path" => "/auth/login\r\n/auth/logout"
+                ],
+                [
+                    "name" => "User setting",
+                    "slug" => "auth.setting",
+                    "http_method" => "GET,PUT",
+                    "http_path" => "/auth/setting"
+                ],
+                [
+                    "name" => "Auth management",
+                    "slug" => "auth.management",
+                    "http_method" => "",
+                    "http_path" => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs"
+                ],
+                [
+                    "name" => "用戶管理",
+                    "slug" => "users",
+                    "http_method" => "",
+                    "http_path" => "/users*"
+                ],
+                [
+                    "name" => "商品管理",
+                    "slug" => "products",
+                    "http_method" => "",
+                    "http_path" => "/products*"
+                ],
+                [
+                    "name" => "優惠券管理",
+                    "slug" => "coupon_codes",
+                    "http_method" => "",
+                    "http_path" => "/coupon_codes*"
+                ],
+                [
+                    "name" => "訂單管理",
+                    "slug" => "orders",
+                    "http_method" => "",
+                    "http_path" => "/orders*"
+                ]
+            ]
+        );
+
+        Role::truncate();
+        Role::insert(
+            [
+                [
+                    "name" => "Administrator",
+                    "slug" => "administrator"
+                ],
+                [
+                    "name" => "運營",
+                    "slug" => "operation"
+                ]
+            ]
+        );
+
+        // pivot tables
+        DB::table('admin_role_menu')->truncate();
+        DB::table('admin_role_menu')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "menu_id" => 2
+                ]
+            ]
+        );
+
+        DB::table('admin_role_permissions')->truncate();
+        DB::table('admin_role_permissions')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "permission_id" => 1
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 2
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 3
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 4
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 6
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 7
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 8
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 9
+                ]
+            ]
+        );
+
+        // finish
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(AdminTablesSeeder::class);
         $this->call(UsersSeeder::class);
         $this->call(UserAddressesSeeder::class);
         $this->call(ProductsSeeder::class);
