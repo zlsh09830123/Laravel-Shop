4e23864fe9c149300a603ef9dbfbe291dfc2cda7
zlsh09830123 <zlsh09830123@gmail.com>
2020-11-23 23:51:59 +0800

    對齊註解

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -34,7 +34,7 @@ class PaymentController extends Controller
     
             //基本參數(請依系統規劃自行調整)
             $MerchantTradeNo = $order->no;
-            $obj->Send['ReturnURL']         = "https://shop.mrhanji.com/callback";      //付款完成通知回傳的網址
+            $obj->Send['ReturnURL']         = "https://shop.mrhanji.com/callback";       //付款完成通知回傳的網址
             $obj->Send['ClientBackURL']     = "https://shop.mrhanji.com/success";        //付款完成通知回傳的網址
             $obj->Send['MerchantTradeNo']   = $MerchantTradeNo;                          //訂單編號
             $obj->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');                       //交易時間
@@ -46,6 +46,9 @@ class PaymentController extends Controller
             //訂單的商品資料
             foreach ($order->items as $item) {
                 array_push($obj->Send['Items'], array('Name' => $item->product->title, 'Price' => (int)$item->price, 'Currency' => "元", 'Quantity' => $item->amount, 'URL' => "dedwed"));
+            }
+            if ($order->couponCode) {
+                array_push($obj->Send['Items'], array('Name' => $order->couponCode->description, 'Price' => 0, 'Currency' => "元", 'Quantity' => '1', 'URL' => "dedwed"));
             }
 
             //產生訂單(auto submit至ECPay)
